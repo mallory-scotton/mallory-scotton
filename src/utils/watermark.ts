9116094c46eff61e928433ce0bdcc2f824bdbe2c
mallory-scotton <mallory.scotton@epitech.eu>
2026-05-07 11:43:08 +0200

    update: simplify watermark content in generated SVG files

--- src/utils/watermark.ts
+++ src/utils/watermark.ts
@@ -1,22 +1,10 @@
-/** Dependencies */
-import fs from 'fs';
-
 /**
  * @brief Watermark for the generated SVG files
  * @description This watermark is added to the generated SVG files to indicate their origin.
  */
 export const WATERMARK = `<!--
-By
- ███▄ ▄███▓ ▄▄▄       ██▓     ██▓     ▒█████   █     █░
-▓██▒▀█▀ ██▒▒████▄    ▓██▒    ▓██▒    ▒██▒  ██▒▓█░ █ ░█░
-▓██    ▓██░▒██  ▀█▄  ▒██░    ▒██░    ▒██░  ██▒▒█░ █ ░█ 
-▒██    ▒██ ░██▄▄▄▄██ ▒██░    ▒██░    ▒██   ██░░█░ █ ░█ 
-▒██▒   ░██▒ ▓█   ▓██▒░██████▒░██████▒░ ████▓▒░░░██▒██▓ 
-░ ▒░   ░  ░ ▒▒   ▓▒█░░ ▒░▓  ░░ ▒░▓  ░░ ▒░▒░▒░ ░ ▓░▒ ▒  
-░  ░      ░  ▒   ▒▒ ░░ ░ ▒  ░░ ░ ▒  ░  ░ ▒ ▒░   ▒ ░ ░  
-░      ░     ░   ▒     ░ ░     ░ ░   ░ ░ ░ ▒    ░   ░  
-       ░         ░  ░    ░  ░    ░  ░    ░ ░      ░    
+Copyright (c) - Mallory SCOTTON. All rights reserved.
+For more information, see https://github.com/mallory-scotton/mallory-scotton/blob/workflow/LICENSE.md
+mailto:mscotton DOT pro AT gmail DOT com
 Generated on ${new Date().toLocaleDateString()}
-
-${fs.readFileSync('LICENSE.md', 'utf-8')}
 -->`;

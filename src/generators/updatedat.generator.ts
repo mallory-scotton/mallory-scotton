03bf27a239f6630162edb8ce0369720a97d20e3f
mallory-scotton <mallory.scotton@epitech.eu>
2026-07-17 16:54:51 +0200

    update: updating Updated At generator to fix the size

--- src/generators/updatedat.generator.ts
+++ src/generators/updatedat.generator.ts
@@ -4,8 +4,9 @@ import { PreferredTheme } from '../types';
 import { SVG } from '../utils';
 
 export function updatedat2svg(userName: string = 'Mallow', theme: PreferredTheme): SVG {
+  const offsetY = 80;
   // Create a new SVG instance
-  const svg = new SVG(250, 20);
+  const svg = new SVG(250, 20 + offsetY);
 
   // Add text to the SVG
   const [bound, _] = svg.addText(UPDATED_AT_TEXT(userName), {
@@ -13,14 +14,14 @@ export function updatedat2svg(userName: string = 'Mallow', theme: PreferredTheme
     lineHeight: 20,
     fontWeight: 'light',
     color: theme === 'dark' ? '#F2F2F2' : '#121212',
-    opacity: 1,
+    opacity: 0.5,
     letterSpacing: 0.2,
-    y: 0,
+    y: 0 + offsetY,
     x: 0
   });
 
   // Resize the SVG width to fit the text
-  svg.setDimensions(Math.ceil(bound.width) + 2, 20);
+  svg.setDimensions(Math.ceil(bound.width) + 2, 20 + offsetY);
 
   // Return the SVG instance
   return svg;
